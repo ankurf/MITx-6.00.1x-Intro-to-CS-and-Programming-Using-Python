ffceca44df747faa02308f0d65a180a242f76f73
ankurf <ankur.falor@gmail.com>
2016-04-09 15:04:56 -0500

    Create Problem_4.py

---
+++ Quiz/Problem_4.py
@@ -0,0 +1,20 @@
+##https://courses.edx.org/courses/course-v1:MITx+6.00.1x_8+1T2016/courseware/19136f9d4de045fdab0ce099230f5385/af94467328b74ed38f0e901460166d70/
+##
+## Problem 4
+##
+##Write a Python function that returns True if aString is a palindrome (reads the same forwards or reversed) and False otherwise. 
+##Do not use Python's built-in reverse function or aString[::-1] to reverse strings.
+
+def isPalindrome(aString):
+    '''
+    aString: a string
+    '''
+    if len(aString)<=1:
+        return True
+    else:
+        while(len(aString)>1):
+            if aString[0]==aString[len(aString)-1]:
+                aString=aString[1:-1]
+            else:
+                return False
+        return True
